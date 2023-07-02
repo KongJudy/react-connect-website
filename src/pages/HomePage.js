d452499a9171b4c110fc9b7bd5b5fc38f21dae2e
Judy Kong <judy.kong817@gmail.com>
2023-07-01 22:31:48 -0400

    home page

---
+++ src/pages/HomePage.js
@@ -0,0 +1,18 @@
+import { Container } from 'reactstrap';
+import PostsList from '../features/posts/PostsList';
+import SubHeader from '../components/subheader/SubHeader';
+
+const HomePage = () => {
+  return (
+    <>
+      <div>
+        <SubHeader current='home' />
+      </div>
+      <Container>
+        <PostsList />
+      </Container>
+    </>
+  );
+};
+
+export default HomePage;
